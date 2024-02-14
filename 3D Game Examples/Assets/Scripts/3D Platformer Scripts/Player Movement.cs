a3a7d323a7e24b2b301b7ea1b3e7bfdb1d522c5c
Drake B <drakeboard719@riversideunified.org>
2024-02-13 23:14:43 -0800

    Started Player Movement and Camera Scripts

---
+++ 3D Game Examples/Assets/Scripts/3D Platformer Scripts/Player Movement.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float turnSpeed = 20f;
+    public float moveSpeed = 1f;
+    public float JumpForce = 10f;
+    public float GravityModifier = 1f;
+    public float OutofBounds = -10f;
+    private float _horizontalInput;
+    private float _forwardInput;
+    public bool IsOnGround = true;
+    Vector3 m_Movement;
+    Rigidbody m_Rigidbody;
+    Quaternion m_Rotation = Quaternion.identity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        m_Movement.Set(horizontal, 0f, vertical);
+        m_Movement.Normalize();
+
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+        m_Rotation = Quaternion.LookRotation(desiredForward);
+
+        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * moveSpeed * Time.deltaTime);
+        m_Rigidbody.MoveRotation(m_Rotation);
+
+        if (Input.GetKeyDown(KeyCode.Space) && IsOnGround)
+        {
+            m_Rigidbody.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
+            IsOnGround = false;
+        }
+
+        Vector3 movement = new Vector3(_horizontalInput, 0.0f, _forwardInput);
+
+        m_Rigidbody.AddForce(movement * moveSpeed);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            IsOnGround = true;
+        }
+    }
+}
